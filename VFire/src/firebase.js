e5279c0248baa800916e311bb5fc893528b1dccc
Aditya Gupta <35148064+aditzkabitz@users.noreply.github.com>
2018-10-18 15:37:13 -0700

    removed sensitive information

--- VFire/src/firebase.js
+++ VFire/src/firebase.js
@@ -1,12 +1,7 @@
 import { initializeApp } from 'firebase';
 
 const app = initializeApp({
-	apiKey: "AIzaSyCE1hmzBBvRowkDkYWmx99YqQ3gLb5Ur7I",
-    authDomain: "share-yourself-artists-183.firebaseapp.com",
-    databaseURL: "https://share-yourself-artists-183.firebaseio.com",
-    projectId: "share-yourself-artists-183",
-    storageBucket: "share-yourself-artists-183.appspot.com",
-    messagingSenderId: "325063782093"
+	//add api key, domain, databaseURL etc.. via firebase acct
 });
 
 export const db = app.database();
